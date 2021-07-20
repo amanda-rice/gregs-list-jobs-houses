b925cd8e150d42492d16f2ef9d807f5f63653a95
amanda-rice <aricericerice@gmail.com>
2021-07-20 16:38:17 -0600

    update some formatting

--- app/Models/Job.js
+++ app/Models/Job.js
@@ -16,9 +16,8 @@ export default class Job {
               <div class="text-center">
                   <p><b>${this.jobTitle}</b></p>
               </div>
-              <p>Job Duties:</p>
               <p>${this.hours} hours per week</p>
-              <p>Preferred Education: ${this.description}</p>
+              <p>Description: ${this.description}</p>
               <p>${this.company}</p>
               <p><em>$${this.rate}/hour</em></p>
               <button class="btn btn-warning btn-block shadow-sm" onclick="app.jobsController.deleteJob('${this.id}')"> delete </button>
